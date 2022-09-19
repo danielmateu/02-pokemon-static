602bf59b4ff1cec831f7e949e8f3e86e4593fb57
Daniel Mateu <danielmateu86@gmail.com>
2022-09-19 13:47:33 +0200

    Se ha eliminado carpeta .next, ya que parece que _document no funciona, a ver si reseteando...

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import cssBaseline from '@nextui-org/react/esm/css-baseline';
+import Document, { Html, Head, Main, NextScript,DocumentContext } from 'next/document';
+
+
+
+
+class MyDocument extends Document {
+
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: <>(initialProps.styles</>
+        }
+    }
+
+    render() {
+        return (
+            <Html lang = 'es'>
+                <Head>
+                   
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
