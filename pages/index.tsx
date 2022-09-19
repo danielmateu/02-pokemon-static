8838fdc3366bd10c2dbbb95893fbd2212291ee9f
Daniel Mateu <danielmateu86@gmail.com>
2022-09-19 12:52:38 +0200

    Se ha realizado la instalación de la libreria NextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@nextui-org/react';
 import type { NextPage } from 'next'
 
 
 const HomePage: NextPage = () => {
   return (
-    <h1>Hola mundo! 😁</h1>
+    <>
+  
+      <Button color='gradient'>Hola mundo! 😁</Button>
+    </>
   )
 }
 
