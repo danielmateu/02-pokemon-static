a8d88c89df403d029cd04512872b90fc5b4a9a14
Daniel Mateu <danielmateu86@gmail.com>
2022-09-20 14:57:04 +0200

    Navegacion y pagina de favs

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,11 +1,13 @@
-import { useTheme, Text, Spacer } from "@nextui-org/react"
+import { useTheme, Text, Spacer, Link } from "@nextui-org/react"
+import NextLink from 'next/link'
+
 import Image from "next/image";
-import { Pokemon } from "../../interfaces";
+
 
 
 export const Navbar = () => {
 
-    const {theme} = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div style={{
@@ -18,18 +20,27 @@ export const Navbar = () => {
             backgroundColor: theme?.colors.gray900.value
         }}>
 
-            <Image 
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png`} 
-            alt='icono de la app' 
-            width={90}
-            height={90}
+            <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png`}
+                alt='icono de la app'
+                width={90}
+                height={90}
             />
 
-            <Text color="white" h1>P</Text>
-            <Text color="white" h3>okémon</Text>
+        <NextLink href='/' passHref>
+            <Link>
+                <Text color="white" h1>P</Text>
+                <Text color="white" h3>okémon</Text>
+            </Link>
+        </NextLink>
 
-            <Spacer css={{flex:1}}/>
-            <Text color="white" >Favoritos</Text>
+            <Spacer css={{ flex: 1 }} />
+
+        <NextLink href='/favorites' passHref>
+            <Link css={{marginRight: '1.5em'}}>
+                <Text color="white">Favoritos</Text>
+            </Link>
+        </NextLink>
 
         </div>
     )
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "../../components/layouts";
+
+
+
+const FavoritesPage = () => {
+
+  return (
+    <Layout title="mis favoritos">
+
+    </Layout>
+  )
+}
+
+export default FavoritesPage;
