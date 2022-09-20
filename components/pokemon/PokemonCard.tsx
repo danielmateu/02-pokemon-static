d63e5747ceb8d447b12ae8736e7574203a4b4296
Daniel Mateu <danielmateu86@gmail.com>
2022-09-20 11:49:40 +0200

    Recibir argumentos por URL y navegacion

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react'
 import { Grid, Card, Row, Text } from '@nextui-org/react';
 
 import { SmallPokemon } from '../../interfaces';
+import { useRouter } from 'next/router';
 
 interface Props {
 
@@ -11,11 +12,24 @@ interface Props {
 }
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
+
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/pokemon/${pokemon.id}`)
+    }
+
     return (
         <Grid key={pokemon.id} xs={6} sm={3} md={2} xl={1}>
-            <Card hoverable clickable>
+            <Card 
+            hoverable 
+            clickable 
+            onClick={onClick}>
                 <Card.Body css={{ p: 1 }}>
-                    <Card.Image src={pokemon.img} width="50%" height="100" />
+                    <Card.Image 
+                    src={pokemon.img} 
+                    width="50%" 
+                    height="100" />
                 </Card.Body>
                 <Card.Footer>
                     <Row justify="space-between">
---
+++ pages/pokemon/[id].tsx
@@ -0,0 +1,26 @@
+
+
+import { useRouter } from 'next/router';
+import { Layout } from '../../components/layouts'
+
+interface Props {
+    pokemon: any;
+}
+
+const PokemonPage = () => {
+
+    const router = useRouter()
+    console.log(router.query);
+
+
+
+    return (
+        <Layout title='Algun pokémon'>
+            <h1>Hola mundo</h1>
+        </Layout>
+    )
+};
+
+
+
+export default PokemonPage;
