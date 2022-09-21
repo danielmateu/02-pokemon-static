1dc499acd75364fd02436e2a0bfaef914ea9ff81
Daniel Mateu <danielmateu86@gmail.com>
2022-09-21 12:11:49 +0200

    Incremental Static Generation Done!

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -116,7 +116,8 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
         paths: pokemonNames.map(name => ({
             params: { name }
         })),
-        fallback: false
+        // fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -124,10 +125,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
     const { name } = params as { name: string };
 
+    const pokemon = await getPokemonInfo(name);
+
+    if(!pokemon){
+        return {
+            redirect:{
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon:await getPokemonInfo(name)
-        }
+            pokemon
+        },
+        revalidate: 86400, //24horas -> 60*60*24
     }
 }
 
